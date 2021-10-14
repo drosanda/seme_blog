012dcabe91b38750edbbad58d6a4fa8e4d45d3ca
Daeng Rosanda <daeng.rosanda@yahoo.co.id>
2021-10-14 08:45:49 +0700

    FEAT: add animate.css

--- app/view/front/home/home.php
+++ app/view/front/home/home.php
@@ -1,13 +1,16 @@
 <div class="container">
   <div class="row mt-2 mb-2">
-    <div class="col-12">
+    <div class="col-12 animate__animated animate__backInDown">
+      <h1 class="">Welcome to <?=$this->config->semevar->site_name?></h1>
+    </div>
+    <div class="col-12 animate__animated animate__backInDown">
       <h2>Kategori Artikel</h2>
     </div>
   </div>
   <div class="row">
   <?php foreach($kategori_list as $kl) {?>
     <div class="col-3 col-md-4">
-      <div class="card mb-2" style="height: 210px;">
+      <div class="card mb-2 animate__animated animate__backInDown" style="height: 210px;">
         <div class="card-body">
           <h5 class="card-title" style="height: 48px;"><?=$kl->nama?></h5>
           <p class="card-text" style="height: 72px;"><?=substr(strip_tags($kl->isi),0,60).'...'?></p>
@@ -20,7 +23,7 @@
   <?php } ?>
   </div>
   <div class="row mt-2 mb-2">
-    <div class="col-12">
+    <div class="col-12 animate__animated animate__backInDown">
       <br>
       <h2>Artikel Terbaru</h2>
     </div>
@@ -28,7 +31,7 @@
   <div class="row">
   <?php foreach($artikel_terbaru as $kl) {?>
     <div class="col-4 col-md-4">
-      <div class="card mb-2" style="height: 210px;">
+      <div class="card mb-2 animate__animated animate__backInDown" style="height: 210px;">
         <div class="card-body">
           <h5 class="card-title" style="height: 48px;"><?=$kl->judul?></h5>
           <p class="card-text" style="height: 72px;"><?=substr(strip_tags($kl->isi),0,100).'...'?></p>
