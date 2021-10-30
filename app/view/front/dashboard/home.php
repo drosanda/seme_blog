718cc6c660311e91e9d64c96515301b44107e91c
Daeng Rosanda <daeng.rosanda@yahoo.co.id>
2021-10-30 17:32:16 +0700

    FEAT: menu by role

--- app/view/front/dashboard/home.php
+++ app/view/front/dashboard/home.php
@@ -3,7 +3,11 @@
     <div class="col s12">
       <h1><small>Selamat Datang kembali,</small> <?=$sess->user->nama?></h1>
 
-      <a href="<?=base_url('artikel/baru')?>" class="btn btn-success"><i class="fa fa-plus"></i> Artikel</a>
+      <?php if($sess->user->role == 'admin' || $sess->user->role == 'owner') { ?>
+      <a href="<?=base_url('artikel/baru')?>" class="btn btn-success">
+        <i class="fa fa-plus"></i> Artikel
+      </a>
+      <?php } ?>
       <a href="<?=base_url('profil')?>" class="btn btn-info"><i class="fa fa-user"></i> Profil</a>
 
     </div>
